fc3431accbb463d82d48533f97843a741eb91be8
kushagra singh tomar <kushagrasingh240033@acropolis.in>
2026-09-01 23:54:54 +0530

    Time: 604 ms (40.48%) | Memory: 281 MB (35.71%) - LeetSync

---
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.java
@@ -0,0 +1,107 @@
+import java.util.*;
+
+class Solution {
+    public int minMoves(String[] classroom, int energy) {
+        int m = classroom.length;
+        int n = classroom[0].length();
+
+        int sr = 0, sc = 0;
+        int litterCount = 0;
+
+        int[][] litterId = new int[m][n];
+
+        for (int[] row : litterId) {
+            Arrays.fill(row, -1);
+        }
+
+        // Find starting position and assign IDs to litter
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char ch = classroom[i].charAt(j);
+
+                if (ch == 'S') {
+                    sr = i;
+                    sc = j;
+                }
+
+                if (ch == 'L') {
+                    litterId[i][j] = litterCount++;
+                }
+            }
+        }
+
+        int allCollected = (1 << litterCount) - 1;
+
+        // row, col, energy, mask, moves
+        Queue<int[]> q = new LinkedList<>();
+
+        q.offer(new int[]{sr, sc, energy, 0, 0});
+
+        boolean[][][][] visited =
+            new boolean[m][n][energy + 1][1 << litterCount];
+
+        visited[sr][sc][energy][0] = true;
+
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            int r = cur[0];
+            int c = cur[1];
+            int e = cur[2];
+            int mask = cur[3];
+            int moves = cur[4];
+
+            // All litter collected
+            if (mask == allCollected) {
+                return moves;
+            }
+
+            // No energy means we cannot make another move
+            if (e == 0) {
+                continue;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+                // Outside grid
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                    continue;
+                }
+
+                // Obstacle
+                if (classroom[nr].charAt(nc) == 'X') {
+                    continue;
+                }
+
+                int ne = e - 1;
+                int nmask = mask;
+
+                // Collect litter
+                if (classroom[nr].charAt(nc) == 'L') {
+                    int id = litterId[nr][nc];
+                    nmask |= (1 << id);
+                }
+
+                // Reset energy
+                if (classroom[nr].charAt(nc) == 'R') {
+                    ne = energy;
+                }
+
+                if (!visited[nr][nc][ne][nmask]) {
+                    visited[nr][nc][ne][nmask] = true;
+
+                    q.offer(new int[]{
+                        nr, nc, ne, nmask, moves + 1
+                    });
+                }
+            }
+        }
+
+        return -1;
+    }
+}
