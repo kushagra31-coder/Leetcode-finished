dc361d6e32819a9630f81cf4cb8f6cc2e5ff1495
kushagra singh tomar <kushagrasingh240033@acropolis.in>
2026-09-04 00:00:00 +0530

    Time: 5 ms (83.33%) | Memory: 121.7 MB (62.32%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min = nums1[0];
+        boolean hasOdd = false;
+
+        for (int x : nums1) {
+            min = Math.min(min, x);
+
+            if (x % 2 != 0) {
+                hasOdd = true;
+            }
+        }
+
+        if (min % 2 == 0 && hasOdd) {
+            return false;
+        }
+
+        return true;
+    }
+}
